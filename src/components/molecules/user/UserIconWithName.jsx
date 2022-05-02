7b24ec24c1dc2bb559dfb9ae60fd58722533090e
AltHiro <103466892+AltHiro@users.noreply.github.com>
2022-05-02 09:00:10 +0900

    a

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import { useContext } from "react";
+import { UserContext } from "../../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
-  const { image, name } = props;
+  const { image, name, isAdmin } = props;
+  const context = useContext(UserContext);
+  console.log(context);
 
   return (
     <Scontainer>
       <SImg heigeth={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </Scontainer>
   );
 };
@@ -24,4 +29,10 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
