0bde4524e6ae963e5f98d25c4ff794dd49d22b91
AltHiro <103466892+AltHiro@users.noreply.github.com>
2022-05-09 09:13:59 +0900

    a

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-import { useLocation } from "react-router-dom";
-
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+//import { useContext } from "react";
+//import { UserContext } from "../../providers/UserProvider";
+
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -20,16 +24,22 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
+  //const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  };
 
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={user.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
